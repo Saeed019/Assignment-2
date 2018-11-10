c9287265cf09451c874817a5677c63b158f4859c
abusa <abusa@DESKTOP-1IOICRI>
2018-11-11 01:00:35 +0600

    Unsorted .cpp and .h added.

---
+++ Assignment 2/PhoneNumber.h
@@ -0,0 +1,19 @@
+#pragma once
+#include<string>
+
+using namespace std;
+struct NumberInfo
+{
+	string mobileNumber;
+	string numberOperator;
+};
+class PhoneNumber
+{
+public:
+	PhoneNumber();
+	~PhoneNumber();
+private:
+
+
+};
+
